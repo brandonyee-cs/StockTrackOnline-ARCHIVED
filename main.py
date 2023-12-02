1c9625085cdd53cd4b8ac6da9aca11aa397defd3
brandonyee-cs <brandonyee.nyc@gmail.com>
2023-12-02 11:41:56 -0500

    Pushed Changes!

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from STOFunctions import SI
-from STOFunctions import Data
+from sto import Data
+from sto import SI
+from sto import AI
 
-ticker = input("Enter stock ticker: ")
-window = int(input("Enter window: "))
-
-stock_df = Data.getPrice(ticker)
+#ticker = input("Enter stock ticker: ")
+#window = int(input("Enter window: "))
+ticker = 'ibm'; window = 15
+stock_df = Data.getData(ticker)
 stock_df = SI.EMA(stock_df, window)
 stock_df = SI.SMA(stock_df, window)
 stock_df = SI.RSI(stock_df, window)
