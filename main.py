caf3e26d31a7d0bc44a3a68ffc14c08442d3b902
brandonyee-cs <brandonyee.nyc@gmail.com>
2023-12-02 14:31:07 -0500

    New Features + Deleted Obsolete Code
    Added Bollinger Bands and factCalc feature, also removed excess STOfunctions file.

--- main.py
+++ main.py
@@ -4,9 +4,7 @@ from sto import AI
 
 #ticker = input("Enter stock ticker: ")
 #window = int(input("Enter window: "))
-ticker = 'ibm'; window = 15
+ticker = 'ibm'; window = 15; k = 2
 stock_df = Data.getData(ticker)
-stock_df = SI.EMA(stock_df, window)
-stock_df = SI.SMA(stock_df, window)
-stock_df = SI.RSI(stock_df, window)
+stock_df = SI.fastCalc(stock_df, ticker,window,k)
 print(stock_df)
